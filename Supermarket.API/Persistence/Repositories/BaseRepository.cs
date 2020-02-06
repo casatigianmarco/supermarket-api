af2e0f6fed87febcae53925c64c371c72bb18553
Gianmarco <gianmarco@grifon.com.br>
2020-02-06 18:25:56 -0300

    In memory database and dependecy injection schema added.

--- Supermarket.API/Persistence/Repositories/BaseRepository.cs
+++ Supermarket.API/Persistence/Repositories/BaseRepository.cs
@@ -9,5 +9,10 @@ namespace Supermarket.API.Persistence.Repositories
     public abstract class BaseRepository
     {
         protected readonly AppDbContext _context;
+
+        public BaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
     }
 }
--- Supermarket.API/Startup.cs
+++ Supermarket.API/Startup.cs
@@ -6,10 +6,16 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Supermarket.API.Domain.Repositories;
+using Supermarket.API.Domain.Services;
+using Supermarket.API.Persistence.Contexts;
+using Supermarket.API.Persistence.Repositories;
+using Supermarket.API.Services;
 
 namespace Supermarket.API
 {
@@ -26,6 +32,13 @@ namespace Supermarket.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<AppDbContext>(options => {
+                options.UseInMemoryDatabase("supermarket-api-in-memory");
+            });
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICategoryService, CategoryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
